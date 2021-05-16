0a2c7bb31a96a0361b34c81490758f1ec5730f81
Daniel Schruhl <daniel.schruhl@hellofresh.com>
2021-05-16 13:20:19 +0200

    Add client to get content

---
+++ src/content/client.test.ts
@@ -0,0 +1,15 @@
+import { getContent } from './client';
+
+describe('content client', () => {
+  it('should return html formatted markdown', () => {
+    const actual = getContent('about-us');
+
+    return expect(actual).resolves.toBeTruthy();
+  });
+
+  it('should throw error on error', () => {
+    const actual = getContent('foo');
+
+    return expect(actual).rejects.toBeTruthy();
+  });
+});
---
+++ src/content/client.ts
@@ -0,0 +1,16 @@
+import { Converter } from 'showdown';
+
+const BASE_URL =
+  'https://raw.githubusercontent.com/Anngei/beyond_srebrenica/main/content/';
+const CONVERTER = new Converter();
+
+export function getContent(resource: string): Promise<string> {
+  return fetch(`${BASE_URL}${resource}.md`)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(response.statusText);
+      }
+      return response.text();
+    })
+    .then((data) => CONVERTER.makeHtml(data));
+}
