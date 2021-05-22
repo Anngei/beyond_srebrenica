9ef0475e0b22cfb6a371e6f7d53bd6e467463102
Daniel Schruhl <danielschruhl@gmail.com>
2021-05-22 12:40:44 +0200

    Add content loading

--- src/components/Content/Content.tsx
+++ src/components/Content/Content.tsx
@@ -1,17 +1,18 @@
 import React, { useEffect, useState } from 'react';
-import { getContent } from '../../content/client';
 import { logger } from '../../logger';
+import { getContent } from './client';
 
-export function BaseContent({ content }: { content: string }): JSX.Element {
+export function Content({ content }: { content: string }): JSX.Element {
   const [html, setHtml] = useState('');
   useEffect(() => {
     getContent(content)
       .then(setHtml)
-      .catch((error) => {
+      .catch((error: any) => {
         logger.error('Error trying to fetch content', error);
         setHtml('Was not able to get content');
       });
   }, [content]);
 
-  return <>{html}</>;
+  // eslint-disable-next-line react/no-danger
+  return <div dangerouslySetInnerHTML={{ __html: html }} />;
 }
--- src/components/Content/client.ts
+++ src/components/Content/client.ts
@@ -4,6 +4,7 @@ const BASE_URL =
   'https://raw.githubusercontent.com/Anngei/beyond_srebrenica/main/content/';
 const CONVERTER = new Converter();
 
+getContent('about-us');
 export function getContent(resource: string): Promise<string> {
   return fetch(`${BASE_URL}${resource}.md`)
     .then((response) => {
