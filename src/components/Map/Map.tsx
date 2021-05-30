5b58c58ab26003976e6286006959ae9c748870ce
Daniel Schruhl <danielschruhl@gmail.com>
2021-05-30 12:42:30 +0200

    Set map center to berlin

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -4,15 +4,15 @@ import { LatLngExpression } from 'leaflet';
 import style from './Map.module.css';
 
 export function Map(_props: any): JSX.Element {
-  const defaultPosition: LatLngExpression = [48.864716, 2.349]; // Paris position
+  const berlinCenter: LatLngExpression = [52.5207797,13.4056501];
 
   return (
-    <MapContainer center={defaultPosition} zoom={13} className={style.map}>
+    <MapContainer center={berlinCenter} zoom={15} className={style.map}>
       <TileLayer
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={defaultPosition}>
+      <Marker position={berlinCenter}>
         <Popup>
           A pretty CSS3 popup. <br /> Easily customizable.
         </Popup>
