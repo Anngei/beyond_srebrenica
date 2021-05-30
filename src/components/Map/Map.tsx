e730c750f04f93f59f257508a204a31a93d9cf7a
Daniel Schruhl <danielschruhl@gmail.com>
2021-05-30 12:51:46 +0200

    Add IKB to map

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,20 +1,32 @@
 import React from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import { LatLngExpression } from 'leaflet';
+import { Link } from 'react-router-dom';
 import style from './Map.module.css';
 
 export function Map(_props: any): JSX.Element {
-  const berlinCenter: LatLngExpression = [52.5207797,13.4056501];
+  const berlinCenter: LatLngExpression = [52.5207797, 13.4056501];
+  const ikbCoordinate: LatLngExpression = [52.5006981, 13.4188505]; // Laden von  https://raw.githubusercontent.com/Anngei/beyond_srebrenica/main/content/poi/ikb-berlin/coordinate.txt
+  const ikbShortInfo =
+    '**Islamisches Kulturzentrum der Bosniaken in Berlin e.V.**\n' +
+    '\n' +
+    'Adalbertstraße 94\n' +
+    '<br />10999 Berlin\n' +
+    '\n' +
+    'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam\n'; // Laden von https://raw.githubusercontent.com/Anngei/beyond_srebrenica/main/content/poi/ikb-berlin/short-info-place.md
 
   return (
-    <MapContainer center={berlinCenter} zoom={15} className={style.map}>
+    <MapContainer center={berlinCenter} zoom={13} className={style.map}>
       <TileLayer
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={berlinCenter}>
+      <Marker position={ikbCoordinate}>
         <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
+          {ikbShortInfo}
+          <p>
+            <Link to="/poi/ikb-berlin">mehr dazu</Link>
+          </p>
         </Popup>
       </Marker>
     </MapContainer>
