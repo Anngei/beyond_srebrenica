d01008be0b6cbe78d773e185fd4ffa4ad882ae80
Daniel Schruhl <daniel.schruhl@gropyus.com>
2022-04-17 09:40:28 +0200

    Fix Button test

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -1,9 +1,14 @@
 import { render, screen } from '@testing-library/react';
 import { Button } from './Button';
+import { MemoryRouter } from 'react-router-dom';
 
 describe('Button component', () => {
   it('should render text of button', () => {
-    render(<Button>foobar</Button>);
+    render(
+      <MemoryRouter>
+        <Button to="someplace">foobar</Button>
+      </MemoryRouter>
+    );
 
     const actual = screen.getByText('foobar');
 
