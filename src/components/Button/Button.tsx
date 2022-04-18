7142fa98b485c26940dcb400d9e563e2ce6239ee
Daniel Schruhl <daniel.schruhl@gropyus.com>
2022-04-18 10:29:12 +0200

    Add icon and types to button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,11 +2,32 @@ import React from 'react';
 import styles from './Button.module.css';
 import { Link } from 'react-router-dom';
 
-export function Button(props: any): JSX.Element {
-  const { to } = props;
+function renderIcon(icon: string | undefined): JSX.Element {
+  if (!icon) {
+    return <></>;
+  }
+  return <i className={icon} style={{paddingRight: "0.8rem"}} />;
+}
+
+export function Button({
+  to,
+  type,
+  icon,
+  children,
+}: {
+  to: string;
+  type?: 'filled' | 'tonal';
+  icon?: string;
+  children?: string;
+}): JSX.Element {
+  const typeClass = type == 'tonal' ? styles.tonal : styles.filled;
+  const iconClass = icon ? styles.withIcon : styles.withoutIcon;
+  const content = icon ? <span className={styles.text}>{children}</span>: children
+
   return (
-    <Link to={to}>
-      <button className={styles.button}>{props.children}</button>
+    <Link to={to} className={`${styles.button} ${typeClass} ${iconClass}`}>
+      {renderIcon(icon)}
+      {content}
     </Link>
   );
 }
