5ab1491860b14d7b03ff11648cc42c00fac32c01
Daniel Schruhl <daniel.schruhl@gropyus.com>
2022-04-18 07:44:15 +0200

    Fix key error

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -5,7 +5,7 @@ import { Content } from './Content/Content';
 
 function renderReport(report: string): JSX.Element {
   return (
-    <>
+    <div key={report}>
       <section className={styles.itemHeading}>
         <Content contentKey={`/reportagen/${report}/name`} />
       </section>
@@ -15,7 +15,7 @@ function renderReport(report: string): JSX.Element {
       <section className={styles.itemLink}>
         <Link to={`/reportage/${report}`}>weiterlesen &#5171;</Link>
       </section>
-    </>
+    </div>
   );
 }
 
