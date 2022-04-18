2df261673b9e3378d98c404ecff964d7188da443
Daniel Schruhl <daniel.schruhl@gropyus.com>
2022-04-18 07:44:02 +0200

    Add hamburger menu

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,8 +1,88 @@
-import React from 'react';
+import React, { Dispatch, useState } from 'react';
 import { Link } from 'react-router-dom';
 import styles from './Navigation.module.css';
+import 'remixicon/fonts/remixicon.css';
+
+interface LinkType {
+  name: string;
+  link: string;
+}
+
+function renderLink(
+  setIsOpen: Dispatch<boolean>,
+  prefix: string
+): (linkItem: LinkType, index: number, links: LinkType[]) => JSX.Element {
+  return (linkItem: LinkType, index: number, links: LinkType[]) => {
+    let customStyle = {};
+    if (index + 1 == links.length) {
+      customStyle = { marginRight: 0 };
+    }
+    return (
+      <div className={styles.hamburgerMenuItem} key={`${prefix}-${index}`}>
+        <Link
+          to={linkItem.link}
+          style={customStyle}
+          onClick={() => setIsOpen(false)}
+        >
+          {linkItem.name}
+        </Link>
+      </div>
+    );
+  };
+}
+
+function renderBurgerMenu(
+  setIsOpen: Dispatch<boolean>,
+  isOpen: boolean,
+  links: LinkType[]
+): JSX.Element {
+  let className = `${styles.hamburgerMenu} ${styles.openedHamburgerMenu}`;
+  if (!isOpen) {
+    className = `${styles.hamburgerMenu}`;
+  }
+  return (
+    <section className={className}>
+      <div className={styles.closeMenu}>
+        <i className="ri-close-line" onClick={() => setIsOpen(!isOpen)} />
+      </div>
+      {links.map(renderLink(setIsOpen, 'burger-menu'))}
+    </section>
+  );
+}
+
+function renderBurgerMenuIcon(
+  setIsOpen: Dispatch<boolean>,
+  isOpen: boolean
+): JSX.Element {
+  if (isOpen) {
+    return <></>;
+  }
+  return (
+    <button
+      className={styles.hamburgerMenuIcon}
+      onClick={() => setIsOpen(!isOpen)}
+    >
+      <i className="ri-menu-line" />
+    </button>
+  );
+}
 
 export function Navigation(_props: any): JSX.Element {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const links = [
+    {
+      name: 'Reportagen',
+      link: '/reportage',
+    },
+    {
+      name: 'Orte',
+      link: '/poi',
+    },
+    {
+      name: 'Hintergrund',
+      link: '/background',
+    },
+  ];
   return (
     <nav className={styles.navigation}>
       <section className={styles.wrapper}>
@@ -10,12 +90,10 @@ export function Navigation(_props: any): JSX.Element {
           Beyond Srebrenica
         </Link>
         <section className={styles.links}>
-          <Link to="/reportage">Reportagen</Link>
-          <Link to="/poi">Orte</Link>
-          <Link to="/background" style={{ marginRight: 0 }}>
-            Hintergrund
-          </Link>
+          {links.map(renderLink(setIsOpen, 'navbar'))}
         </section>
+        {renderBurgerMenuIcon(setIsOpen, isOpen)}
+        {renderBurgerMenu(setIsOpen, isOpen, links)}
       </section>
     </nav>
   );
