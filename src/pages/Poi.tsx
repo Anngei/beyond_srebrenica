6bec065d20b913a79bfd1ac6563d6ec0eaa54ca6
Daniel Schruhl <daniel.schruhl@gropyus.com>
2022-04-18 10:31:48 +0200

    Add back button to poi page

--- src/pages/Poi.tsx
+++ src/pages/Poi.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
 import { Content } from '../components/Content/Content';
 import { useParams } from 'react-router-dom';
+import { Button } from '../components/Button/Button';
 
 export function Poi(_props: any): JSX.Element {
   const { poiId } = useParams<{ poiId: string }>();
-  return <Content contentKey={`poi/${poiId}/description`} />;
+  return (
+    <>
+      <Button to="../" icon="ri-arrow-left-s-line" type="tonal">
+        zurück
+      </Button>
+      <Content contentKey={`poi/${poiId}/description`} />
+    </>
+  );
 }
